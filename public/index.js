8368c32f50cff1a8c12d6326ba8898b6b281985f
manoj <chapagainmanoj35@gmail.com>
2017-10-30 16:55:52 +0545

    refactor

---
+++ public/index.js
@@ -0,0 +1,39 @@
+// receivedMessage (event) .
+// handleMessageAttachments (messageAttachments, senderID)
+// handleQuickReply  (senderID, quickReply, messageId)
+// handleEcho  (messageId, appId, metadata)
+// handleApiAiAction  (sender, action, responseText, contexts, parameters)
+// handleMessage(message, sender)
+// handleCardMessages(messages, sender)
+// handleApiAiResponse(sender, response)
+// sendToApiAi(sender, text)
+// sendTextMessage(recipientId, text)
+// sendImageMessage(recipientId, imageUrl)
+// sendGifMessage(recipientId)
+// sendAudioMessage(recipientId)
+// sendVideoMessage(recipientId, videoName)
+// sendFileMessage(recipientId, fileName)
+// sendButtonMessage(recipientId, text, buttons)
+// sendGenericMessage(recipientId, elements)
+// sendReceiptMessage(recipientId, recipient_name, currency, payment_method,
+// 							timestamp, elements, address, summary, adjustments)
+// sendQuickReply(recipientId, text, replies, metadata)
+// sendReadReceipt(recipientId)
+// sendTypingOn(recipientId)
+// sendTypingOff(recipientId)
+// sendAccountLinking(recipientId)
+// greetUserText(userId)
+// callSendAPI(messageData)
+//
+// // post backevent
+// receivedPostback(event) .
+// receivedMessageRead(event) .
+// receivedAccountLink(event) .
+// receivedDeliveryConfirmation(event) .
+//
+// // Authorization event
+// receivedAuthentication(event) .
+//
+// verifyRequestSignature(req, res, buf)
+//
+// isDefined(obj)
